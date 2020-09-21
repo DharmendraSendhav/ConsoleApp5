39ee2c866e5999c1e0896535413b8715149ac09d
dharmendrasendhav@outlook.com <dharmendrasendhav@outlook.com>
2020-09-21 22:05:55 +0530

    new changes

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+
+        static Stopwatch stopwatch = new Stopwatch();
+        static void Main(string[] args)
+        {
+            
+            Program p = new Program();
+            p.DispTask();
+           
+            Console.WriteLine("hello" + Thread.CurrentThread.ManagedThreadId);
+
+            Console.ReadLine();
+        }
+
+        public void DispTask()
+        {
+
+            Console.WriteLine("disp" + Thread.CurrentThread.ManagedThreadId);
+            Task t = test();
+         //   t.Wait(100077);
+           
+            // using await control will go to caller method and here it will suspend . once task completed again control will be here.
+            //await t;
+            
+            Console.WriteLine("disp after vali "+Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public async Task test()
+        {
+           // stopwatch.Start();
+            // using await control will go to caller method (DispTask) and here it will suspend . once task completed again control will be here.
+            await Task.Delay(5000);
+            stopwatch.Stop();
+            Console.WriteLine("test "+Thread.CurrentThread.ManagedThreadId+" elapsed time"+stopwatch.ElapsedMilliseconds);
+        }
+
+    }
+}
